035f46b3084cb4efda0338c832b0ebd4fe604b5b
ismailhelmy <38408394+ismailhelmy@users.noreply.github.com>
2019-03-11 20:58:37 +0200

    Removed build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/D/CUFE CHS/Semester 8/Machine Intelligence/omni-scrabbler
-# Build directory: D:/D/CUFE CHS/Semester 8/Machine Intelligence/omni-scrabbler/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
