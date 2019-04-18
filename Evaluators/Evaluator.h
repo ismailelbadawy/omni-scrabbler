eebf0ffaa9c2fb035422bafb8f6eeecb69c89812
Ismail El Badawy <ismailelbadawyw@gmail.com>
2019-04-19 01:27:08 +0200

    Added an abstract class for Evaluators.

---
+++ Evaluators/Evaluator.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../Models/Move.h"
+
+/// The abstract class (contract) for any Evaluator to implement
+class Evaluator
+{
+    public:
+    virtual double Evaluate(Move * move);
+}
